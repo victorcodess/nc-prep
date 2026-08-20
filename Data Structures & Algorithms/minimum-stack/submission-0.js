1f5db8c8bc1cd9188610b0a681650050418e47d3
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-08-20 22:42:19 +0100

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,35 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        return this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        const min = Math.min(...this.stack);
+        return min;
+    }
+}
