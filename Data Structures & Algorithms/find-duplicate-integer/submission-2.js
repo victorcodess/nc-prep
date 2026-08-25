6bcbc8e7c99b56da4102954bcb60db41a9415915
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-08-25 13:53:40 +0100

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) { // Time: O(n), Space: O(1)
+        // meet fast and slow1
+        let slow1 = 0, fast = 0;
+
+        while (true) {
+            slow1 = nums[slow1];
+            fast = nums[nums[fast]];
+
+            if (slow1 === fast) break;
+        }
+
+        // meet slow 2 and slow
+        let slow2 = 0;
+
+        while (true) {
+            slow1 = nums[slow1];
+            slow2 = nums[slow2];
+
+            if (slow1 === slow2) break;
+        }
+
+        // return slow
+        return slow1;
+    }
+}
