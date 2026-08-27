02f7c12d4a7377cbc88cea6c47630765dd2c900d
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-08-27 11:45:41 +0100

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[][]}
+     */
+    levelOrder(root) { // Time: O(n), Space: O(n)
+        if (!root) return [];
+
+        const queue = [[root, 0]];
+        let head = 0
+
+        const result = [];
+
+        while (head < queue.length) {
+            const [curr, level] = queue[head++];
+
+            if (!result[level]) result[level] = [];
+            result[level].push(curr.val);
+
+            if (curr.left) queue.push([curr.left, level + 1]);
+            if (curr.right) queue.push([curr.right, level + 1]);
+        }
+
+        return result;
+    }
+}
