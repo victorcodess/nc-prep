69f459666fd7db1acdeeb6b2401a9c3bca02bfbc
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-08-27 10:23:59 +0100

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestor(root, p, q) { // Time: O(logn), Space: O(1)
+        let curr = root;
+
+        while (curr) {
+            if (curr.val < p.val && curr.val < q.val) {
+                curr = curr.right;
+            } else if (curr.val > p.val && curr.val > q.val) {
+                curr = curr.left;
+            } else {
+                return curr;
+            }
+        }
+    }
+}
