e38891dbcd11aa12de95fa870155a12f68ee67ce
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-08-28 16:01:05 +0100

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) { // Time: O(logn), Space: O(logn)
+        const stoneHeap = new MaxPriorityQueue();
+
+        for (const stone of stones) {
+            stoneHeap.enqueue(stone);
+        }
+
+        while (stoneHeap.size() > 1) {
+            const x = stoneHeap.dequeue();
+            const y = stoneHeap.dequeue();
+            const diff = Math.abs(x - y);
+
+            if (diff > 0) {
+                stoneHeap.enqueue(diff);
+            }
+        }
+
+        return stoneHeap.front() ?? 0;
+    }
+}
