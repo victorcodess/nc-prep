d0fd5a822265e45e59334245b46303087b09bbd9
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-01 12:11:58 +0100

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.js
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string[][]}
+     */
+    partition(s) { // Time: O(2 ** n), Space: O(n)
+        const result = [];
+        const parts = [];
+
+        this.findPalins(s, parts, result, 0);
+
+        return result;
+    }
+
+    findPalins(s, parts, result, start) {
+        if (start >= s.length) {
+            result.push([...parts]);
+            return;
+        }
+
+        let i = start;
+        // for (let i = start; i < s.length; i++) {
+            for (let j = i; j < s.length; j++) {
+                if (this.isPalin(s, i, j)) {
+                    parts.push(s.slice(i, j + 1)); 
+                    this.findPalins(s, parts, result, j + 1);
+                    parts.pop();
+                }
+            }
+        // }
+
+        return;
+    }
+
+    isPalin(s, i, j) {
+        while (i <= j) {
+            if (s[i] !== s[j]) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
