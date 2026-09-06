31743e1d4781ddbf7db56e1c42e8ccd3cab7d8b6
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-06 23:42:34 +0100

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums, i = 0, hasFirst = false, memo = new Map()) { // Time: O(n), Space: O(n)
+        if (i >= nums.length) return 0;
+        if (hasFirst && i === nums.length - 1) return 0;
+        const key = String(i) + String(hasFirst);
+        if (memo.has(key)) return memo.get(key);
+
+        const curr = nums[i];
+        const tookFirst = i === 0 ? true : hasFirst;
+
+        const doTake = curr + this.rob(nums, i + 2, tookFirst, memo);
+        const dontTake = this.rob(nums, i + 1, hasFirst, memo);
+
+        memo.set(key, Math.max(doTake, dontTake));
+        return memo.get(key);
+    }
+}
