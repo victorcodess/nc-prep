ca3a5cdaa87d4d59cae95e50d6ac7c2dabd993e3
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-08 09:58:27 +0100

    Add: longest-increasing-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-4.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) { // Time: O(n), Space: O(n)
+        if (!nums.length) return 0;
+
+        let maxSeq = 1;
+        const memo = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            const currSeq = this.findSeq(nums, i, memo);
+            maxSeq = Math.max(maxSeq, currSeq);
+        }
+
+        return maxSeq;
+    }
+
+    findSeq(nums, i, memo) {
+        if (memo.has(i)) return memo.get(i);
+        if (i >= nums.length) return 0;
+
+        let maxSeq = 1;
+
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] <= nums[i]) continue;
+
+            const candSeq = 1 + this.findSeq(nums, j, memo);
+            maxSeq = Math.max(maxSeq, candSeq);
+        }
+
+        memo.set(i, maxSeq);
+        return memo.get(i);
+    }
+}
