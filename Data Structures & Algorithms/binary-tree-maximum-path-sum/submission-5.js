6c180cdf83cef781d216d6d52f7a0002825f6986
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-19 03:39:09 +0100

    Add: binary-tree-maximum-path-sum - submission-5

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-5.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxPathSum(root) { // Time: O(n), Space: O(h)
+        function findMax(root) {
+            if (!root) return 0;
+
+            const leftMax = findMax(root.left);
+            const rightMax = findMax(root.right);
+
+            const currMax = Math.max(root.val, root.val + leftMax, root.val + rightMax);
+
+            maxPath = Math.max(maxPath, currMax, root.val + leftMax + rightMax);
+            
+            return currMax;
+        }
+
+        let maxPath = -Infinity;
+        findMax(root);
+
+        return maxPath;
+    }
+}
